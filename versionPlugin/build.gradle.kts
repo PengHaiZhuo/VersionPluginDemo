44109f29b163c96c48ba104f80c6582032411195
haizhuo <penghaizhuo@foxmail.com>
2021-01-22 17:56:52 +0800

    normal commit

--- versionPlugin/build.gradle.kts
+++ versionPlugin/build.gradle.kts
@@ -31,7 +31,9 @@ kotlinDslPluginOptions {
 gradlePlugin {
     plugins {
         create("version") {
+            //自定义plugin的id，其他module引用要用到
             id = "com.haizhuo.version"
+            //指向自定义plugin类，因为我直接放在目录下，所以没有多余路径
             implementationClass = "VersionConfigPlugin"
         }
     }
